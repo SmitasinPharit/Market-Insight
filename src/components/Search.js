1a609b2800792265a92bd1edd080945729c2b697
Pharit Smitasin <smitasinpharit@yahoo.com>
2023-07-30 18:53:39 -0400

    dark mode implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,56 +1,66 @@
-import React, { useState } from 'react';
-import { mockSearchResults } from '../constants/mock';
+import React, { useContext, useState } from "react";
+import { mockSearchResults } from "../constants/mock";
 import { XIcon, SearchIcon } from "@heroicons/react/solid";
 import SearchResults from "./SearchResults";
+import ThemeContext from "../context/ThemeContext";
+import { searchSymbol } from "../../clone/stock-dashboard/src/utils/api/stock-api";
 
 const Search = () => {
-    const [input, setInput] = useState("");
-    const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
+  const [input, setInput] = useState("");
+  const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
 
-    const clear = () => {
-        setInput("");
-        setBestMatches([]);
-    };
+  const { darkMode } = useContext(ThemeContext);
 
-    const updateBestMatches = () => {
-        setBestMatches(mockSearchResults.results);
-    };
+  const clear = () => {
+    setInput("");
+    setBestMatches([]);
+  };
 
-    return (
-        <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
-            <input 
-            type="text" 
-            value={input} 
-            className="w-full px-4 py-2 focus:outline-none rounded-md"
-            placeholder="Search stock..."
-            onChange={(event) => {
-                setInput(event.target.value)
-            }}
-            onKeyPress={(event) => {
-                if (event.key === "Enter") {
-                    updateBestMatches();
-                }
-            }}
-            />
+  const updateBestMatches = () => {
+    setBestMatches(mockSearchResults.results);
+  };
 
-            {input && (
-                <button onClick={clear} className="m-1">
-                    <XIcon className="h-4 w-4 fill-gray-500"/> 
-                </button>
-            )}
-            
-            <button 
-                onClick={updateBestMatches} 
-                className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2"
-            >
-                <SearchIcon className="h-4 w-4 fill-gray-100"/>
-            </button>
+  return (
+    <div
+      className={`flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white ${
+        darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
+      }`}
+    >
+      <input
+        type="text"
+        value={input}
+        className={`w-full px-4 py-2 focus:outline-none rounded-md ${
+          darkMode ? "bg-gray-900" : null
+        }`}
+        placeholder="Search stock..."
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            updateBestMatches();
+          }
+        }}
+      />
 
-            {input && bestMatches.length > 0 ? (
-            <SearchResults results={bestMatches}/>
-            ) : null}
-        </div>
-    )
-}
+      {input && (
+        <button onClick={clear} className="m-1">
+          <XIcon className="h-4 w-4 fill-gray-500" />
+        </button>
+      )}
 
-export default Search
+      <button
+        onClick={updateBestMatches}
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2"
+      >
+        <SearchIcon className="h-4 w-4 fill-gray-100" />
+      </button>
+
+      {input && bestMatches.length > 0 ? (
+        <SearchResults results={bestMatches} />
+      ) : null}
+    </div>
+  );
+};
+
+export default Search;
